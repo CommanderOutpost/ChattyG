4018e10eef5f06720e2c729dfb8f600be96cb687
rasheed <thcmofskot@gmail.com>
2024-02-16 17:32:33 +0100

    sets secret key and django_allowed_hosts as env variables

--- sentiment_analysis_app/sentiment_analysis_app/settings.py
+++ sentiment_analysis_app/sentiment_analysis_app/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,13 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ze4ov*e0qw5@1hp*_k(zy1pbo4%ni1dyus*tanjmf6_3(k5*!o"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(",")
 
 # Application definition
 
