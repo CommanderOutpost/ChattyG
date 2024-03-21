e801c1f13afcfce862c95d6d6fde99f56b159eb7
Rasheed Salami <136803958+CommanderOutpost@users.noreply.github.com>
2024-03-21 19:09:05 +0100

    empties allowed hosts

--- sentiment_analysis_app/sentiment_analysis_app/settings.py
+++ sentiment_analysis_app/sentiment_analysis_app/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = []
 
 # Application definition
 
